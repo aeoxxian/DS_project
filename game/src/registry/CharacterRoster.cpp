90abeea069ce90037353922e010c924123eb85e1
Inkang Jeong <wjddlsrkd@kentech.ac.kr>
2026-06-10 19:40:18 +0900

    test

--- game/src/registry/CharacterRoster.cpp
+++ game/src/registry/CharacterRoster.cpp
@@ -36,10 +36,10 @@ void CharacterRoster::printAll() const {
 void registerAllCharacters() {
     CharacterRoster& r = CharacterRoster::instance();
 
-    r.registerCharacter(CharacterDef(1, "이민준", "Nuclear 트랙",     100, 0, 0, Track::Nuclear));
-    r.registerCharacter(CharacterDef(2, "김서연", "AI 트랙",          100, 0, 0, Track::AI));
-    r.registerCharacter(CharacterDef(3, "박준서", "Grid 트랙",        100, 0, 0, Track::Grid));
-    r.registerCharacter(CharacterDef(4, "최유진", "NewMaterial 트랙", 100, 0, 0, Track::NewMaterial));
-    r.registerCharacter(CharacterDef(5, "정하은", "Hydrogen 트랙",    100, 0, 0, Track::Hydrogen));
-    r.registerCharacter(CharacterDef(6, "한승준", "EcoTech 트랙",     100, 0, 0, Track::EcoTech));
+    r.registerCharacter(CharacterDef(1, "이민준", "Nuclear",     100, 0, 0, Track::Nuclear));
+    r.registerCharacter(CharacterDef(2, "김서연", "AI",          100, 0, 0, Track::AI));
+    r.registerCharacter(CharacterDef(3, "박준서", "Grid",        100, 0, 0, Track::Grid));
+    r.registerCharacter(CharacterDef(4, "최유진", "NewMaterial", 100, 0, 0, Track::NewMaterial));
+    r.registerCharacter(CharacterDef(5, "정하은", "Hydrogen",    100, 0, 0, Track::Hydrogen));
+    r.registerCharacter(CharacterDef(6, "한승준", "EcoTech",     100, 0, 0, Track::EcoTech));
 }
